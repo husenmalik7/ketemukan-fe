db6109daae34b4af78a932f9819af853df2e6e78
Husen <husenmalik7@gmail.com>
2025-07-05 19:14:58 +0700

    +

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,18 @@ function Navbar() {
   return (
     <header className="flex justify-between items-center px-4 bg-red-100 h-12">
       <div className="flex gap-4">
-        <h1>Logo Ketemukan</h1>
+        <h1>
+          <Link to="/">Logo Ketemukan</Link>
+        </h1>
 
         <nav>
           <ul className="flex gap-4">
-            <li>lost</li>
-            <li>found</li>
+            <li>
+              <Link to="/lost">Lost</Link>
+            </li>
+            <li>
+              <Link to="/found">found</Link>
+            </li>
           </ul>
         </nav>
       </div>
