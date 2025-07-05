3f869521b7aa9da638ee364eeb9d5855c062652b
Husen <husenmalik7@gmail.com>
2025-07-05 19:52:35 +0700

    create: login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -2,6 +2,16 @@ function LoginPage() {
   return (
     <section>
       <p>LoginPage page</p>
+
+      <form className="bg-green-200 flex flex-col p-5">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+
+        <button type="submit">Login</button>
+      </form>
     </section>
   );
 }
