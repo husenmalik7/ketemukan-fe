324130910ee4d7ce6841c4d057b92b53054dce1e
Husen <husenmalik7@gmail.com>
2025-07-06 08:24:55 +0700

    create: lost and found page

--- src/pages/FoundPage.jsx
+++ src/pages/FoundPage.jsx
@@ -1,7 +1,41 @@
 function FoundPage() {
+  const dummyItem = [
+    {
+      id: 'id-satu',
+      title: 'ditemukan dompet warna hitam',
+      short_desc: 'ditemukan dompet di dufan, warna hitam',
+      description: 'jika ada yang menemukan dompet, mohon hubungi nomor saya, atau datang ke rumah',
+      picture: 'link_picture',
+      lostDate: '09-09-2000',
+      createdAt: '2000',
+      updatedAt: '2020',
+      userId: 'id-user-satu',
+    },
+    {
+      id: 'id-dua',
+      title: 'ditemukan dompet warna putih',
+      short_desc: 'ditemukan dompet di dufan, warna hitam',
+      description: 'jika ada yang menemukan dompet, mohon hubungi nomor saya, atau datang ke rumah',
+      picture: 'link_picture',
+      lostDate: '09-09-2000',
+      createdAt: '2000',
+      updatedAt: '2020',
+      userId: 'id-user-satu',
+    },
+  ];
   return (
     <section>
       <p>FoundPage page</p>
+
+      <div className="bg-orange-300 h-120">
+        {dummyItem.map((item, index) => (
+          <div key={index} className="item bg-orange-100 pb-12">
+            <p>{item.id}</p>
+            <p>{item.title}</p>
+            <p>{item.short_desc}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
--- src/pages/LostPage.jsx
+++ src/pages/LostPage.jsx
@@ -1,7 +1,42 @@
 function LostPage() {
+  const dummyItem = [
+    {
+      id: 'id-satu',
+      title: 'hilang dompet warna hitam',
+      short_desc: 'hilang dompet di dufan, warna hitam',
+      description: 'jika ada yang menemukan dompet, mohon hubungi nomor saya, atau datang ke rumah',
+      picture: 'link_picture',
+      lostDate: '09-09-2000',
+      createdAt: '2000',
+      updatedAt: '2020',
+      userId: 'id-user-satu',
+    },
+    {
+      id: 'id-dua',
+      title: 'hilang dompet warna putih',
+      short_desc: 'hilang dompet di dufan, warna hitam',
+      description: 'jika ada yang menemukan dompet, mohon hubungi nomor saya, atau datang ke rumah',
+      picture: 'link_picture',
+      lostDate: '09-09-2000',
+      createdAt: '2000',
+      updatedAt: '2020',
+      userId: 'id-user-satu',
+    },
+  ];
+
   return (
     <section>
       <p>LostPage page</p>
+
+      <div className="bg-orange-300 h-120">
+        {dummyItem.map((item, index) => (
+          <div key={index} className="item bg-orange-100 pb-12">
+            <p>{item.id}</p>
+            <p>{item.title}</p>
+            <p>{item.short_desc}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
