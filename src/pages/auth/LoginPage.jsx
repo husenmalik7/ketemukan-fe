00c036eb4b9bebf623d12a42285253607bc4f211
Husen <husenmalik7@gmail.com>
2025-07-13 21:33:08 +0700

    integrate: login user

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,14 +1,38 @@
+import useInput from '../../hooks/useInput';
+import { login } from '../../utils/api/auth';
+
 function LoginPage() {
+  const [username, onUsernameChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+
+  async function onLoginHandler(event) {
+    event.preventDefault();
+
+    // setIsLoading(true);
+    try {
+      const { error, data } = await login({ username, password });
+      if (!error) {
+        console.log(data);
+        // loginSuccess(data);
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Terjadi kesalahan pada server');
+    } finally {
+      // setIsLoading(false);
+    }
+  }
+
   return (
     <section>
       <p>LoginPage page</p>
 
-      <form className="bg-green-200 flex flex-col p-5">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+      <form onSubmit={onLoginHandler} className="bg-green-200 flex flex-col p-5">
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" value={username} onChange={onUsernameChange} />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" value={password} onChange={onPasswordChange} />
 
         <button type="submit">Login</button>
       </form>
--- src/utils/api/auth/index.js
+++ src/utils/api/auth/index.js
@@ -1,7 +1,6 @@
 import BASE_URL from '../../config';
 
 async function register({ username, password, fullname }) {
-  console.log(username, password, fullname);
   const response = await fetch(`${BASE_URL}/users`, {
     method: 'POST',
     headers: {
@@ -20,4 +19,23 @@ async function register({ username, password, fullname }) {
   return { error: false };
 }
 
-export { register };
+async function login({ username, password }) {
+  const response = await fetch(`${BASE_URL}/authentications`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+}
+
+export { register, login };
