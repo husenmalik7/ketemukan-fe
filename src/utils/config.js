38c3f5cfe295bf19984bf5d370e042bb78082caf
Husen <husenmalik7@gmail.com>
2025-07-24 16:09:56 +0700

    change baseURL to local

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://ketemukan-be-production-7e9f.up.railway.app';
+// const BASE_URL = 'https://ketemukan-be-production-7e9f.up.railway.app';
+const BASE_URL = 'http://localhost:5000';
 
 export default BASE_URL;
