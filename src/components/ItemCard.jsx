1fb5e07d470e50276ea84f0c35d812ec02a98e27
Husen <husenmalik7@gmail.com>
2025-07-27 08:03:55 +0700

    1

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -8,31 +8,6 @@ import { MdOutlineDeleteOutline } from 'react-icons/md';
 import { showFormattedDate } from '../utils';
 import { NavLink } from 'react-router-dom';
 
-const RenderMyItemPicture = ({ img }) => {
-  return img ? (
-    <img
-      src={img}
-      className="h-full w-full object-cover transition-transform duration-500 hover:scale-105"
-      alt="avatar"
-    />
-  ) : (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-24 text-red-300"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-      />
-    </svg>
-  );
-};
-
 function ItemCard({
   id,
   title,
@@ -49,6 +24,31 @@ function ItemCard({
     lost: 'bg-orange-500',
     found: 'bg-green-500',
     resolved: 'bg-gray-500',
+  };
+
+  const RenderMyItemPicture = ({ img }) => {
+    return img ? (
+      <img
+        src={img}
+        className="h-full w-full object-cover transition-transform duration-500 hover:scale-105"
+        alt="avatar"
+      />
+    ) : (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-24 text-red-300"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+        />
+      </svg>
+    );
   };
 
   return (
