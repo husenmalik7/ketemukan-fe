217c9861f70253b78092323a71ead24da7c40384
Husen <husenmalik7@gmail.com>
2025-07-27 08:54:57 +0700

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 function HomePage() {
   return (
-    <section>
+    <section className="flex min-h-screen flex-col pb-20">
       <p>home page</p>
       <p className="text-3xl font-bold underline">Hello world!</p>
-      <p className="text-xl font-bold ">Hello world!</p>
+      <p className="text-xl font-bold">Hello world!</p>
     </section>
   );
 }
