0ec8209cd850373ec9da738045ffa814a00f5723
Husen <husenmalik7@gmail.com>
2025-07-28 21:38:31 +0700

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -64,20 +64,15 @@ function HomePage() {
             </p>
           </div>
 
-          <div className="col-span-1 mx-12 flex flex-col items-center justify-center rounded-lg bg-orange-100 p-4 shadow-lg transition-transform duration-200 hover:-translate-y-1 md:mx-4 md:p-6">
-            <p className="text-3xl font-bold text-pink-400">9</p>
-            <p className="text-xl font-medium text-gray-500">Eletronik</p>
-          </div>
-
-          <div className="col-span-1 mx-12 flex flex-col items-center justify-center rounded-lg bg-orange-100 p-4 shadow-lg transition-transform duration-200 hover:-translate-y-1 md:mx-4 md:p-6">
-            <p className="text-3xl font-bold text-pink-400">9</p>
-            <p className="text-xl font-medium text-gray-500">Eletronik</p>
-          </div>
-
-          <div className="col-span-1 mx-12 flex flex-col items-center justify-center rounded-lg bg-orange-100 p-4 shadow-lg transition-transform duration-200 hover:-translate-y-1 md:mx-4 md:p-6">
-            <p className="text-3xl font-bold text-pink-400">9</p>
-            <p className="text-xl font-medium text-gray-500">Eletronik</p>
-          </div>
+          {mostLostedCategories.map((item, index) => (
+            <div
+              key={index}
+              className="col-span-1 mx-12 flex flex-col items-center justify-center rounded-lg bg-orange-100 p-4 shadow-lg transition-transform duration-200 hover:-translate-y-1 md:mx-4 md:p-6"
+            >
+              <p className="text-3xl font-bold text-pink-400">{item.total}</p>
+              <p className="text-xl font-medium text-gray-500">{item.category_name}</p>
+            </div>
+          ))}
         </div>
 
         <div className="my-4 grid grid-cols-1 gap-4 bg-lime-300 md:my-12 md:grid-cols-3">
