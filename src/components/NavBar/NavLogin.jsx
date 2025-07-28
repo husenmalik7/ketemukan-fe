0a88a722eb7214795665234b9c0c5de093462220
Husen <husenmalik7@gmail.com>
2025-07-28 19:44:34 +0700

    refactor: simplify NavLogin component structure

--- src/components/NavBar/NavLogin.jsx
+++ src/components/NavBar/NavLogin.jsx
@@ -3,10 +3,8 @@ import NavLinkItem from './NavLinkItem';
 
 function NavLogin() {
   return (
-    <div className="relative ml-auto flex items-center space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
-      <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse">
-        <NavLinkItem name="Login" to="/login" />
-      </ul>
+    <div className="list-none font-medium">
+      <NavLinkItem name="Login" to="/login" />
     </div>
   );
 }
