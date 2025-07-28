70c2e5ec83ead02ffc34c46e4912a1fb9858e53a
Husen <husenmalik7@gmail.com>
2025-07-28 16:45:22 +0700

    1

--- src/pages/LostPage.jsx
+++ src/pages/LostPage.jsx
@@ -16,8 +16,30 @@ function LostPage() {
   }, []);
 
   return (
-    <section className="flex min-h-screen flex-col pb-20">
-      <div className="m-4 mt-24 grid grid-cols-1 gap-4 md:mt-12 md:grid-cols-2 lg:grid-cols-3">
+    <section className="flex min-h-screen flex-col bg-orange-200 pb-20">
+      <form className="m-4 mt-24 grid grid-cols-1 gap-4 bg-orange-300 md:mt-12 md:grid-cols-6">
+        <div className="col-span-1 md:col-span-2">
+          <input
+            id="id"
+            type="text"
+            // value={value}
+            // onChange={onChange}
+            className="block w-full rounded-sm border border-gray-300 bg-gray-50 p-2.5 text-sm font-medium text-gray-900 focus:outline-gray-300"
+            placeholder="Judul Item"
+          />
+        </div>
+
+        <div className="col-span-1">
+          <button
+            type="submit"
+            className="w-full cursor-pointer rounded-lg bg-gradient-to-r from-red-400 via-red-500 to-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:ring-4 focus:ring-red-300 focus:outline-none"
+          >
+            Cari Item
+          </button>
+        </div>
+      </form>
+
+      <div className="m-4 mt-12 grid grid-cols-1 gap-4 bg-orange-400 md:grid-cols-2 lg:grid-cols-3">
         {losts.map((item, index) => (
           <div key={index}>
             <ItemCard
