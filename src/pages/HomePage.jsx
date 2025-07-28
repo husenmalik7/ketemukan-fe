e34b5ba2429277e8fe63345bf70c2216ac9cf2a5
Husen <husenmalik7@gmail.com>
2025-07-29 06:59:25 +0700

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -30,24 +30,24 @@ function HomePage() {
   }, []);
 
   return (
-    <section className="flex min-h-screen flex-col bg-orange-200 pb-20">
+    <section className="flex min-h-screen flex-col pb-6">
       {/* hero */}
-      <div className="flex flex-col items-center justify-center bg-lime-200 p-4 pb-12">
+      <div className="flex flex-col items-center justify-center p-4 pb-12">
         <img src="/images/ketemukan.png" className="w-64" alt="Logo Ketemukan" />
-        <p className="max-w-xl bg-orange-100 text-center text-3xl font-bold text-[#FB7A7C]">
+        <p className="max-w-xl text-center text-3xl font-bold text-[#FB7A7C]">
           Temukan Barangmu, Bantu Temukan Barang Orang Lain
         </p>
-        <p className="mt-2 max-w-xl bg-orange-300 text-center text-xl font-medium text-gray-500">
+        <p className="mt-2 max-w-xl text-center text-xl font-medium text-gray-500">
           Cari dan laporkan barang hilang atau ditemukan dengan mudah. Bersama Ketemukan, pasti bisa
           ketemu-kan
         </p>
       </div>
 
       {/* statistic */}
-      <div className="bg-red-700">
-        <div className="my-4 grid grid-cols-1 gap-4 bg-lime-300 md:my-12 md:grid-cols-3">
+      <div className="bg-red-500">
+        <div className="my-4 grid grid-cols-1 gap-4 md:my-12 md:grid-cols-3">
           <div className="col-span-1 md:col-span-3">
-            <p className="my-2 text-center text-2xl font-bold text-gray-500">
+            <p className="my-2 text-center text-2xl font-bold text-white">
               Lokasi Kehilangan Paling Sering
             </p>
           </div>
@@ -63,9 +63,9 @@ function HomePage() {
           ))}
         </div>
 
-        <div className="my-4 grid grid-cols-1 gap-4 bg-lime-300 md:my-12 md:grid-cols-3">
+        <div className="my-4 grid grid-cols-1 gap-4 md:my-12 md:grid-cols-3">
           <div className="col-span-1 md:col-span-3">
-            <p className="my-2 text-center text-2xl font-bold text-gray-500">
+            <p className="my-2 text-center text-2xl font-bold text-white">
               Kategori Paling Sering Dilaporkan
             </p>
           </div>
@@ -81,9 +81,9 @@ function HomePage() {
           ))}
         </div>
 
-        <div className="my-4 grid grid-cols-1 gap-4 bg-lime-300 md:my-12 md:grid-cols-3">
+        <div className="my-4 grid grid-cols-1 gap-4 md:my-12 md:grid-cols-3">
           <div className="col-span-1 md:col-span-3">
-            <p className="my-2 text-center text-2xl font-bold text-gray-500">Top Contributor</p>
+            <p className="my-2 text-center text-2xl font-bold text-white">Top Contributor</p>
           </div>
 
           {/* // TODO conditional if no image */}
@@ -95,7 +95,7 @@ function HomePage() {
                   className="h-24 w-24 rounded-full object-cover"
                   alt="Logo Ketemukan"
                 />
-                <p className="text-xl font-medium text-gray-500">{item.username}</p>
+                <p className="text-xl font-medium text-white">{item.username}</p>
               </div>
             </div>
           ))}
@@ -103,9 +103,9 @@ function HomePage() {
       </div>
 
       {/* recent item */}
-      <div>
-        <p className="text-center text-2xl font-bold text-gray-500">Recent Item</p>
-        <div className="mx-2 grid grid-cols-1 gap-4 bg-lime-300 md:grid-cols-4">
+      <div className="mt-12">
+        <p className="mb-6 text-center text-2xl font-bold text-gray-500">Recent Item</p>
+        <div className="mx-2 grid grid-cols-1 gap-4 md:grid-cols-4">
           {recentItems.map((item, index) => (
             <ItemCard
               key={index}
@@ -125,42 +125,43 @@ function HomePage() {
       </div>
 
       {/* cara kerja */}
-      <div className="grid grid-cols-1 gap-4 bg-lime-400 md:grid-cols-3">
+      <div className="mt-12 grid grid-cols-1 gap-4 bg-red-500 p-4 pt-6 pb-8 md:grid-cols-3">
         <div className="col-span-1 md:col-span-3">
-          <p className="text-center text-2xl font-bold text-gray-500">Gimana cara kerjanya?</p>
+          <p className="text-center text-2xl font-bold text-white">Gimana cara kerjanya?</p>
         </div>
 
-        <div className="flex flex-col items-center bg-indigo-200 text-center">
+        <div className="flex flex-col items-center text-center">
           <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600">
             <FaMagnifyingGlass className="text-2xl text-white" />
           </div>
-          <p className="mt-2 text-xl font-medium">Post Item</p>
-          <p className="text-gray-500">
+          <p className="mt-2 text-xl font-medium text-white">Post Item</p>
+          <p className="text-white">
             Laporkan barang hilangmu atau unggah barang yang kamu temukan dengan deskripsi yang
             jelas
           </p>
         </div>
 
-        <div className="flex flex-col items-center bg-indigo-200 text-center">
+        <div className="flex flex-col items-center text-center">
           <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-br from-green-400 via-green-500 to-green-600">
             <BsPeopleFill className="text-2xl text-white" />
           </div>
-          <p className="mt-2 text-xl font-medium">Comment</p>
-          <p className="text-gray-500">
+          <p className="mt-2 text-xl font-medium text-white">Comment</p>
+          <p className="text-white">
             Bantu mereka dengan membagikan informasi dan meninggalkan komentar yang berguna
           </p>
         </div>
 
-        <div className="flex flex-col items-center bg-indigo-200 text-center">
+        <div className="flex flex-col items-center text-center">
           <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-br from-amber-400 via-amber-500 to-amber-600">
             <FaCoins className="text-2xl text-white" />
           </div>
-          <p className="mt-2 text-xl font-medium">Points</p>
-          <p className="text-gray-500">Dapatkan point dari pelaporan dan komentar yang membantu</p>
+          <p className="mt-2 text-xl font-medium text-white">Points</p>
+          <p className="text-white">Dapatkan point dari pelaporan dan komentar yang membantu</p>
         </div>
       </div>
 
-      <div className="m-6 flex flex-col items-center rounded-2xl bg-gradient-to-br from-pink-400 via-pink-500 to-pink-600 p-6 text-center">
+      {/* create your account */}
+      <div className="m-6 flex flex-col items-center rounded-2xl bg-gradient-to-br from-orange-100 via-orange-200 to-orange-300 p-6 text-center">
         <p className="text-3xl font-bold">Siap untuk Memulai?</p>
         <p className="text-lg font-medium">
           Bergabunglah dengan kami dan bantu mengembalikan barang hilang kepada pemiliknya
@@ -168,7 +169,7 @@ function HomePage() {
         <div>
           <button
             type="button"
-            className="cursor-point mt-6 rounded-lg bg-gradient-to-r from-red-400 via-red-500 to-red-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:ring-4 focus:ring-red-300 focus:outline-none"
+            className="mt-6 cursor-pointer rounded-lg bg-gradient-to-r from-red-400 via-red-500 to-red-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:ring-4 focus:ring-red-300 focus:outline-none"
           >
             Buat akunmu
           </button>
