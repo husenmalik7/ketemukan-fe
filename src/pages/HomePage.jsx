7d05653e9359363a1f191c5f81e86c53f80a7f16
Husen <husenmalik7@gmail.com>
2025-07-28 20:10:32 +0700

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 function HomePage() {
   return (
-    <section className="flex min-h-screen flex-col pb-20">
-      <p>home page</p>
-      <p className="text-3xl font-bold underline">Hello world!</p>
-      <p className="text-xl font-bold">Hello world!</p>
+    <section className="flex min-h-screen flex-col bg-orange-200 pb-20">
+      <div className="ketemukan-hero flex flex-col items-center justify-center bg-lime-200 p-4">
+        <img src="/images/ketemukan.png" className="w-64" alt="Logo Ketemukan" />
+        <p className="max-w-xl bg-orange-100 text-center text-3xl font-bold text-[#FB7A7C]">
+          Temukan Barangmu, Bantu Temukan Barang Orang Lain
+        </p>
+        <p className="mt-2 max-w-xl bg-orange-300 text-center text-xl font-medium text-gray-500">
+          Cari dan laporkan barang hilang atau ditemukan dengan mudah. Bersama Ketemukan, pasti bisa
+          ketemu-kan
+        </p>
+      </div>
     </section>
   );
 }
