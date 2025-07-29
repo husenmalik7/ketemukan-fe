28a3807506a8b9c18babe8f94784c3fcd9408260
Husen <husenmalik7@gmail.com>
2025-07-29 13:05:43 +0700

    feat: responsive profile page

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -5,7 +5,7 @@ import { MdOutlineDateRange } from 'react-icons/md';
 import { formatJoinedDate } from '../../utils';
 
 import Cat from '../../assets/Cat';
-import { GrUploadOption } from 'react-icons/gr';
+import { FaCamera } from 'react-icons/fa';
 
 const RenderProfilePicture = ({ img }) => {
   return img ? (
@@ -38,15 +38,17 @@ function ProfileCard({
   }
 
   return (
-    <div className="flex rounded-lg p-4 shadow-[0_4px_8px_rgba(0,0,0,0.1),0_-4px_8px_rgba(0,0,0,0.1)] hover:shadow-[0_6px_12px_rgba(0,0,0,0.15),0_-6px_12px_rgba(0,0,0,0.15)] hover:transition-shadow hover:duration-300">
-      <div>
+    <div className="grid grid-cols-1 rounded-lg p-4 shadow-[0_4px_8px_rgba(0,0,0,0.1),0_-4px_8px_rgba(0,0,0,0.1)] hover:shadow-[0_6px_12px_rgba(0,0,0,0.15),0_-6px_12px_rgba(0,0,0,0.15)] hover:transition-shadow hover:duration-300 md:grid-cols-8">
+      {/* avatar */}
+      <div className="col-span-1">
         <RenderProfilePicture img={picture_url} />
         <div>
-          <GrUploadOption
-            title="Edit profil picture"
+          <div
             onClick={handleClickInputFile}
-            className="cursor-pointer text-2xl text-orange-400"
-          />
+            className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-gray-200"
+          >
+            <FaCamera title="Edit profil picture" className="text-[#FB7A7C]" />
+          </div>
           <input
             onChange={onEditProfileImage}
             type="file"
@@ -57,12 +59,13 @@ function ProfileCard({
         </div>
       </div>
 
-      <div>
+      {/* information */}
+      <div className="col-span-1 mt-4 md:col-span-4 md:mt-0">
         <p className="text-3xl font-bold">{fullname}</p>
         <p className="text-lg text-gray-500">{username}</p>
 
-        <div className="my-3 flex">
-          <div className="mr-4 flex items-center">
+        <div className="my-3 grid grid-cols-2 md:grid-cols-4">
+          <div className="flex items-center">
             <IoLocationOutline className="mr-0.5 text-gray-500" />
             <p className="text-gray-500">{location_name}, Indonesia</p>
           </div>
@@ -73,22 +76,22 @@ function ProfileCard({
           </div>
         </div>
 
-        <div className="flex">
-          <div className="mr-4 flex flex-col items-center justify-center">
+        <div className="grid grid-cols-2 md:grid-cols-4">
+          <div className="flex flex-col items-center justify-center">
             <p className="text-2xl font-bold text-blue-500">{lostCount}</p>
             <p className="text-gray-500">Lost Items</p>
           </div>
-          <div className="mr-4 flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
             <p className="text-2xl font-bold text-green-500">{foundCount}</p>
             <p className="text-gray-500">Found Items</p>
           </div>
-          <div className="ml-6 flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
             <p className="text-2xl font-bold text-orange-500">{points}</p>
             <p className="text-gray-500">Points</p>
           </div>
           <div
             onClick={() => setOpenModalAchievement(true)}
-            className="ml-6 flex cursor-pointer flex-col items-center justify-center duration-500 hover:scale-105"
+            className="flex cursor-pointer flex-col items-center justify-center duration-500 hover:scale-105"
             title="Lihat achievement saya"
           >
             <p className="animate-colorChange bg-gradient-to-r from-pink-500 via-yellow-500 to-purple-500 bg-clip-text text-2xl font-bold text-transparent">
@@ -99,7 +102,7 @@ function ProfileCard({
         </div>
       </div>
 
-      <div className="ml-auto">
+      <div className="mt-2 md:col-start-8 md:mt-0">
         <button
           onClick={() => setOpenModalEditProfile(true)}
           type="button"
