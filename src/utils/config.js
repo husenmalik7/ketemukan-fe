ac40de940c1928a97d688847dbce1c2f921ab017
Husen <husenmalik7@gmail.com>
2025-08-01 05:55:41 +0700

    deploy final locked

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'https://ketemukan-be-production-7e9f.up.railway.app';
-// const BASE_URL = 'https://ketemukan-be-production.up.railway.app';
+const BASE_URL = 'https://ketemukan-be-production.up.railway.app';
 
-const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000';
 
 export default BASE_URL;
